c864e76d4c67c6bfeec036ad8f0cf927eab996e8
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-04-06 13:04:53 +0330

    Refactoring getters and setters

--- src/domain/models/Role/role.ts
+++ src/domain/models/Role/role.ts
@@ -1,39 +1,42 @@
 import { CRUD } from "../../interfaces/event";
 
 export class Role {
-  constructor(public id: number, public name: "admin" | "reviewer" | "editor") {
-    this.id = id;
-    this.name = name;
+  private _id: number;
+  private _name: "admin" | "reviewer" | "editor";
+
+  constructor(id: number, name: "admin" | "reviewer" | "editor") {
+    this._id = id;
+    this._name = name;
   }
 
-  public getId(): number {
-    return this.id;
+  public get id(): number {
+    return this._id;
   }
 
-  public getName(): string {
-    return this.name;
+  public get name(): string {
+    return this._name;
   }
 
   public toJSON(): object {
     return {
-      id: this.id,
-      name: this.name,
+      id: this._id,
+      name: this._name,
     };
   }
 
-  public create(role: Role) {
+  public create(role: Role): CRUD {
     const event = new CRUD();
     event.create({ title: "create", data: role, occured_at: new Date() });
     return event; // This is a mock implementation
   }
 
-  public update(role: Role) {
+  public update(role: Role): CRUD {
     const event = new CRUD();
     event.update({ title: "update", data: role, occured_at: new Date() });
     return event; // This is a mock implementation
   }
 
-  public delete(role: Role) {
+  public delete(role: Role): CRUD {
     const event = new CRUD();
     event.delete({ title: "delete", data: role, occured_at: new Date() });
     return event; // This is a mock implementation
